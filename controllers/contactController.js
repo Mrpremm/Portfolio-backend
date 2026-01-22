4535041c5dbbbc65342e42b64d2becfba2c2d439
Prem Prasad <premrinkinoniya@gmail.com>
2026-01-23 00:43:47 +0530

    Updated API logic

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,6 +1,7 @@
 import Contact from "../models/Contact.js";
+import sendEmail from "../utils/sendEmail.js";
 
-// @desc   Save contact message
+// @desc   Save contact message + send email
 // @route  POST /api/contact
 // @access Public
 export const sendContactMessage = async (req, res, next) => {
@@ -12,16 +13,19 @@ export const sendContactMessage = async (req, res, next) => {
       throw new Error("All fields are required");
     }
 
+    // 1️⃣ Save to MongoDB
     const contact = await Contact.create({
       name,
       email,
       message,
     });
 
+    // 2️⃣ Send email to YOU
+    await sendEmail({ name, email, message });
+
     res.status(201).json({
       success: true,
       message: "Message sent successfully",
-      data: contact,
     });
   } catch (error) {
     next(error);
